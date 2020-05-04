18a39f6dc67dab7b8f087895a4aea30de8bcb4a0
loc-nt <48140302+loc-nt@users.noreply.github.com>
2020-05-04 00:07:39 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,22 +5,15 @@ import re
 from sqlalchemy import create_engine
 from config import db_password
 import sys
-import psycopg2
+# !{sys.executable} -m pip install psycopg2-binary
 import time
 import Challenge as ch
 
 file_dir = "C:/Users/LocNguyen/OneDrive - stok LLC/Documents/Loc Nguyen/Online Data Analytics Bootcamp/Module 8/Movies-ETL/Resources/"
 
-# Load JSON:
-with open(f'{file_dir}/wikipedia.movies.json', mode='r') as file:
-    wiki_raw = json.load(file)
-# Load csv
-kaggle_raw = pd.read_csv(f'{file_dir}movies_metadata.csv')
-ratings_raw = pd.read_csv(f'{file_dir}ratings.csv')
+# file paths - user providing:
+wiki_path = f'{file_dir}/wikipedia.movies.json'
+kaggle_path = f'{file_dir}movies_metadata.csv'
+ratings_path = f'{file_dir}ratings.csv'
 
-# database link: "postgres://[user]:[password]@[location]:[port]/[database]"
-db_string = f"postgres://postgres:{db_password}@127.0.0.1:5432/movie_data"
-# Create the database engine with the following:
-engine = create_engine(db_string)
-
-ch.master_ETL(wiki_raw, kaggle_raw, ratings_raw)
+ch.master_ETL(wiki_path, kaggle_path, ratings_path)
